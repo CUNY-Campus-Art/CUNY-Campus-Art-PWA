107b862de0edb3ac3c01f90bc098321199a76c36
Mary Leong <mleong011@gmail.com>
2020-12-12 20:57:51 -0500

    (style) added components for if(qr scanned === art clue artwork) as buttons for now

--- src/components/HuntClues.tsx
+++ src/components/HuntClues.tsx
@@ -12,6 +12,7 @@ import {
   IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
+  IonAlert,
 } from "@ionic/react";
 import {
   arrowBackCircleOutline,
@@ -21,34 +22,46 @@ import {
   warning,
   wifi,
   wine,
+  colorPalette,
+  medalOutline,
+  searchOutline,
+  trophy,
+  bulbOutline,
 } from "ionicons/icons";
+import { Icon } from "ionicons/dist/types/components/icon/icon";
 
 const HuntClues = () => {
+  
   const [showModal, setShowModal] = useState(false);
+  const [showCorrectAlert, setShowCorrectAlert] = useState(false);
+  const [showIncorrectAlert, setShowIncorrectAlert] = useState(false);
   return (
     <div>
       <IonList>
-        {/* Possibly map clues to list : {items.map((image, i) => (<IonItem key = {i}> <IonImg src = {image.src}/> </IonItem>))} */}
+        {/* Possibly map clues to list : {artworks.map((artworks.clue, i) => (<IonItem key = {i}> <IonImg src = {artworks.clue}/> </IonItem>))} */}
         <IonItem>
           {/* <IonThumbnail slot="start">
           <img src= {require("../assets/images/bluesClues.jpg")} />
         </IonThumbnail> */}
           <IonLabel>
             {/* CATEGORY */}
-            <h3>Modern Art</h3>
+            <h3>artworks.artwork_type_clue: Modern Art</h3>
             {/* AMOUNT OF POINTS AWARDED IF SOLVED */}
-            <p>10 POINTS</p>
+            <p>artworks.points: 10 POINTS</p>
             {/* THE CLUE */}
-            <p>Find me near a water fountain in the music department</p>
+            <p>artworks.clue: Find me near a water fountain in the music department</p>
           </IonLabel>
+
+          {/* modal component showing the clue and buttons to solve or go back */}
           <IonModal isOpen={showModal}>
             {/* repeated information */}
-            <h3>Modern Art</h3>
-            <p>10 POINTS</p>
-            <p>Find me near a water fountain in the music department</p>
+            <h3>artworks.artwork_type_clue: Modern Art</h3>
+            <p>artworks.points: 10 POINTS</p>
+            <p>artworks.clue: Find me near a water fountain in the music department</p>
             {/* to do <IonButton>Scan Artwork</IonButton> */}
             <IonButton onClick={() => setShowModal(false)}>Go Back</IonButton>
           </IonModal>
+
           <IonButton onClick={() => setShowModal(true)}>Solve</IonButton>
         </IonItem>
 
@@ -65,16 +78,19 @@ const HuntClues = () => {
               west coast of South America in the 1700s.
             </p>
           </IonLabel>
+          <IonButton onClick={() => setShowModal(true)}>Solve</IonButton>
 
+
+        {/* ION MODAL COMPONENT (POP UP FOR CLUE): */}
           <IonModal isOpen={showModal}>
             <IonCard>
               <IonCardHeader>
-                <IonCardSubtitle>Category</IonCardSubtitle>
+                <IonCardSubtitle>Category <IonIcon icon={colorPalette}></IonIcon></IonCardSubtitle>
                 <IonCardTitle>New World and Old World cultures</IonCardTitle>
               </IonCardHeader>
 
               <IonCardContent className="clue">
-                <IonCardSubtitle> Clue </IonCardSubtitle>
+                <IonCardSubtitle> Clue <IonIcon icon={bulbOutline}></IonIcon></IonCardSubtitle>
                 <IonCardTitle>
                   The scene may show a rescue from a pirate attack, common along
                   the west coast of South America in the 1700s.
@@ -82,7 +98,7 @@ const HuntClues = () => {
               </IonCardContent>
 
               <IonCardHeader>
-                <IonCardSubtitle> Award </IonCardSubtitle>
+                <IonCardSubtitle> Award <IonIcon icon={medalOutline}></IonIcon></IonCardSubtitle>
                 <IonCardTitle> 10 Points </IonCardTitle>
               </IonCardHeader>
 
@@ -101,11 +117,75 @@ const HuntClues = () => {
                   <IonIcon icon={arrowBackCircleOutline} slot="start" />
                   <IonLabel>Go Back</IonLabel>
                 </IonItem>
+
+                {/* temporary ion items to be implemented when scan qr logic is added: */}
+                <IonItem
+                  button
+                  color="danger"
+                  onClick={() => setShowCorrectAlert(true)}
+                >
+                  <IonIcon icon={arrowBackCircleOutline} slot="start" />
+                  <IonLabel>IF CORRECT</IonLabel>
+                </IonItem>
+
+                <IonItem
+                  button
+                  color="danger"
+                  onClick={() => setShowIncorrectAlert(true)}
+                >
+                  <IonIcon icon={arrowBackCircleOutline} slot="start" />
+                  <IonLabel>IF INCORRECT</IonLabel>
+                </IonItem>
+
+
               </IonList>
             </IonCard>
-          </IonModal>
-          
-          <IonButton onClick={() => setShowModal(true)}>Solve</IonButton>
+          </IonModal> 
+          {/* END OF MODAL */}
+         
+
+          {/* if the scan of qr code is correct show this alert: possibly add image to message*/}
+          <IonAlert
+          isOpen={showCorrectAlert}
+          onDidDismiss={() => { setShowCorrectAlert(false); setShowModal(false);}}
+          header={'CORRECT!'} 
+          subHeader={'congratulations'}
+          message={"You have scannrd the correct artwork!  {/*number of points*/} has been added to your account" }
+          buttons={[
+            {
+             text: 'Close',
+             handler: () =>{
+               console.log("confirm close");
+             }
+            }
+            ]}
+        /> {/*ion alert - correct*/}
+
+        {/* if the scan of qr code is correct show this alert: */}
+          <IonAlert
+          isOpen={showIncorrectAlert}
+          onDidDismiss={() => { setShowIncorrectAlert(false) }}
+          header={'SORRY, INCORRECT! '}
+          subHeader={'Artwork scanned is incorrect'}
+          message={"you have selected the incorrect artwork! Please try again" }
+          buttons={[
+            {
+             text: 'Exit to Clues',
+             role: 'cancel',
+             handler: () =>{
+               console.log("confirm close");
+               setShowModal(false);
+             }
+            },
+             {
+              text: 'Try Again',
+              handler: () =>{
+                console.log("confirm try again");
+                setShowModal(true);
+              }
+            }
+            ]}
+        /> {/*ion alert - incorrect*/}
         </IonItem>
       </IonList>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,15 +48,9 @@ const Home: React.FC = () => {
                 <p>
                   {" "}
                   CUNY Gallery was created to showcase CUNY students' artwork in
-                  an accessible way through the scanning of QR codes. Lorem
-                  ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                  enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  an accessible way through the scanning of QR codes located on artwork
+                  throughout different campuses. Students can submit their artwork and information
+                  using our form site and can be showcased on our application. 
                 </p>
               </div>
             </IonRow>
